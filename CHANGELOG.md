02ba146a142a9cbdd5c605af1e781644b82278cb
Victor Ling <86419+Ionshard@users.noreply.github.com>
2025-03-22 18:04:31 -0400

    v0.2.0 (#1)
    * Add devcontainer
    
    * Make Cargo.lock a committed file
    
    * Update edition
    
    * Update dependencies
    
    * Setup log level to verbosity arg
    
    * Refactor and add proper logging
    
    * Add config file
    
    * All shell expansion on directory
    
    * Final output
    
    * Allow saving a save directory
    
    * Prepare for v0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG
+
+## v0.2.0
+
+Changes
+- Proper logging
+- Read directory from a config file
+- Adds new `--save-directory` argument to create the config file
+
+## v0.1.0
+
+Initial Version
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,152 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, anyhow};
 use arboard::Clipboard;
 use chrono::Local;
 use clap::Parser;
-use image::RgbaImage;
-use log::info;
-use std::path::PathBuf;
+use clap_verbosity_flag::Verbosity;
+use config::Config;
+use image::{ImageBuffer, Rgba, RgbaImage};
+use log::{debug, error, info, trace};
+use std::{path::PathBuf, process, env::current_dir, fs};
+use serde::Deserialize;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[clap(flatten)]
-    verbose: clap_verbosity_flag::Verbosity,
+    verbose: Verbosity,
+
     #[arg(short, long)]
+    directory: Option<PathBuf>,
+
+    #[arg(long)]
+    save_directory: Option<String>,
+}
+
+#[derive(Debug, Default, Deserialize, PartialEq, Eq)]
+struct AppConfig {
     directory: Option<PathBuf>,
 }
 
-fn filename(directory: &PathBuf) -> PathBuf {
+fn setup_logging(verbose: Verbosity) -> Result<()> {
+    if let Some(level) = verbose.log_level() {
+        simple_logger::init_with_level(level)?;
+    }
+
+    Ok(())
+}
+
+fn get_config_home() -> Result<PathBuf> {
+    if let Some(dir) = dirs::config_dir() {
+        return Ok(dir);
+    }
+    Ok(current_dir()?)
+}
+
+fn get_config_dir() -> Result<PathBuf> {
+    Ok(get_config_home()?.join("clipsaver"))
+}
+
+fn get_config() -> Result<AppConfig> {
+
+    let config_dir = get_config_dir()?;
+    debug!("Looking for config in {:?}", config_dir);
+
+    let config_name = config_dir.join("config");
+    let config_name = config_name.to_str().ok_or(anyhow!("Failed to parse {:?}", config_name))?;
+
+    let config = Config::builder()
+    .add_source(config::File::with_name(config_name).required(false))
+    .add_source(config::Environment::with_prefix("CLIPSAVER"))
+    .build()?
+    .try_deserialize::<AppConfig>()?;
+
+    trace!("Config: {:?}", config);
+
+    Ok(config)
+}
+
+fn get_save_directory(args: &Args, config: &AppConfig) -> Result<PathBuf> {
+
+    let save_directory = args.directory.clone().or_else(|| config.directory.clone()).unwrap_or(PathBuf::from("."));
+
+    let x = shellexpand::path::full(&save_directory)?;
+
+    return Ok(PathBuf::from(x));
+
+}
+
+fn get_image_from_clipboard() -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>> {
+    let mut clipboard = Clipboard::new().context("Attempted to get clipboard")?;
+
+    let image = clipboard
+        .get_image()
+        .context("Getting image from clipboard")?;
+    
+    RgbaImage::from_raw(
+        image.width as u32,
+        image.height as u32,
+        image.bytes.to_vec(),
+    )
+    .context("Could not parse image")
+}
+
+fn get_save_filename(directory: &PathBuf) -> PathBuf {
     let now = Local::now();
     let filename = format!("Clipboard {}.png", now.format("%Y-%m-%d_%H.%M.%S"));
     directory.join(filename)
 }
 
-fn main() -> Result<()> {
-    simple_logger::init()?;
+fn save_image_to_file(
+    image: ImageBuffer<Rgba<u8>, Vec<u8>>,
+    filename: &PathBuf,
+) -> Result<()> {
+    info!("Saving image to {:?}", filename);
+    image.save(filename).context("Trying to save image")
+}
+
+fn save_directory(directory: String) -> Result<()> {
+    let config_dir = get_config_dir()?;
+    let filename = config_dir.join("config.ini");
+
+    debug!("Setting configuration in {:?}", filename);
+
+    fs::create_dir_all(config_dir)?;
+    fs::write(filename, format!("directory = {}", directory))?;
+
+    println!("Set save directory to {}", directory);
+
+    process::exit(0);
+}
+
+fn clipsaver() -> Result<PathBuf> {
     let args = Args::parse();
-    let mut clipboard = Clipboard::new()?;
-    let image = clipboard.get_image()?;
-    let img = RgbaImage::from_raw(
-        image.width as u32,
-        image.height as u32,
-        image.bytes.to_vec(),
-    )
-    .context("Could not parse image")?;
-    let directory = args.directory.unwrap_or(PathBuf::from("."));
-    let file = filename(&directory);
-    info!("Saving image to {}", file.display());
-    img.save(file)?;
-    Ok(())
+    setup_logging(args.verbose)?;
+
+    trace!("Command Line: {:?}", args);
+
+    if let Some(dir) = &args.save_directory {
+        save_directory(dir.clone())?
+    }
+
+    let config = get_config()?;
+
+    let directory = get_save_directory(&args, &config)?;
+
+    info!("Saving to directory: {:?}", directory);
+
+    let filename = get_save_filename(&directory);
+
+    let image = get_image_from_clipboard()?;
+    save_image_to_file(image, &filename)?;
+
+    Ok(filename)
+}
+
+fn main() {
+    let file = clipsaver().unwrap_or_else(|error| {
+        error!("Failed to save clipboard: {:?}", error);
+        process::exit(1);
+    });
+
+    println!("Saved image from clipboard to {:?}", file)
 }
